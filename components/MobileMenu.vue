5d56afa766d8e2a7e82914f5194cb1fca5341fcd
renreso <renresozz@gmail.com>
2020-09-01 21:41:41 +0900

    add: menuとmobilemenuの追加

---
+++ components/MobileMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mobile-menu">
+		<div class="hamburger_menu">
+			<img src="../assets/icons/hamburger_menu.svg" alt="hamburger_menu" class="menu_icon" />
+    </div>
+  </div>
+</template>
+<style>
+.mobile-menu {
+  width: 100%;
+  background: #191970;
+}
+
+.mobile-menu img {
+  width: 3.2rem;
+  height: 3.2rem;
+  padding: 0.8rem;
+}
+</style>
