a7851626d5c7e95e80cf6b63e1fa63d1ff6b83e9
renreso <renresozz@gmail.com>
2020-09-03 23:05:29 +0900

    add: profileコンテンツの追加

---
+++ components/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="profile">
+		<p>{{profile.data}}</p>
+	</div>
+</template>
+<style>
+.profile {
+	text-align: center;
+}
+</style>
+<script>
+import profile from "../assets/contents/profile.json"
+export default {
+	data: () => {
+		return({profile: profile})
+	}
+}
+</script>
